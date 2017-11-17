a29679218dcb9e9f2c27d5e013611a2573f19b15
Christian Quast <christian@quast-datentechnik.de>
2017-11-17 12:04:59 +0100

    Initial angular configuration

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tus-schedule',
+  templateUrl: './app.component.html'
+})
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
+    BrowserModule,
     CommonModule
   ],
-  declarations: []
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
