5368c1169fc6d3122823976018d696724060f505
Christian Quast <christian@quast-datentechnik.de>
2017-11-22 11:37:21 +0100

    added json db provider

--- server/server.js
+++ server/server.js
@@ -4,27 +4,40 @@ const fs = require('fs');
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const low = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileAsync');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'POST');
-    next();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  next();
 });
 
-app.post('/', (req, res) => {
+const adapter = new FileAsync('db.json');
+low(adapter).then(db => {
+
+  app.post('/', (req, res) => {
     console.log('Received request');
-    fs.writeFile('json.json', JSON.stringify(req.body), (err) => {
-        if (err) throw err;
-        console.log('File written to json.json');
-        res.json(req.body);
-        // res.send('File written to json.json');
-    })
-});
+    db.get('teams')
+      .push(req.body)
+      .last()
+      .write()
+      .then(team => res.json(team));
+    // fs.writeFile('json.json', JSON.stringify(req.body), (err) => {
+    //   if (err) throw err;
+    //   console.log('File written to json.json');
+    //   res.json(req.body);
+    // })
+  });
 
-app.listen(3000, () => {
+  return db.defaults({ teams: [] }).write();
+}).then(() => {
+
+  app.listen(3000, () => {
     console.log('Listening on port 3000. Post a file to http://localhost:3000 to save to /json.json');
+  });
 });
