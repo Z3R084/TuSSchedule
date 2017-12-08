5fa44129af1e93ef14eb35bbde518ed7a5eeda37
Christian Quast <christian@quast-datentechnik.de>
2017-12-08 14:23:41 +0100

    added schedule creation

--- src/app/admin/mode/mode.component.ts
+++ src/app/admin/mode/mode.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { TournamentService } from '../../services/tournament.service';
 import { Tournament } from '../../models/tournament';
+import { Team } from '../../models/team';
 
 @Component({
   selector: 'tus-mode',
@@ -32,11 +33,62 @@ export class ModeComponent {
   }
 
   create() {
-    this.tournamentService.addTournamentDb(this.tournament).subscribe();
+    if (this.mode === 'league') {
+      this.generateRoundRobinTournamentSchedule();
+    } else {
+      this.generateKoTournamentSchedule();
+    }
+    // this.tournamentService.addTournamentDb(this.tournament).subscribe();
   }
 
   update() {
     this.tournamentService.updateTournamentDb(this.tournament).subscribe();
+  }
+
+  private generateRoundRobinTournamentSchedule() {
+    let schedule = [];
+    const teams = this.tournament.teams.filter((team, index) => {
+      return team.league === 1;
+    });
+
+
+    if (teams.length % 2 === 1) {
+      const dummy: Team = { name: 'Spielfrei', league: 1 };
+      teams.push(dummy);
+    }
+
+    const rounds = teams.length - 1;
+    
+    for (let i = 0; i < rounds; i++) {
+      schedule[i] = [];
+      for (let j = 0; j < teams.length / 2; j++) {
+        schedule[i].push([teams[j].name, teams[rounds - j].name]);
+      }
+      teams.splice(1, 0, teams.pop());
+    }
+    console.log(schedule);
+  }
+
+  private generateKoTournamentSchedule() {
+    let schedule = [];
+    const teams = this.tournament.teams;
+
+    if (teams.length % 2 === 1) {
+      const dummy: Team = { name: 'Freilos', league: 1 };
+      teams.push(dummy);
+    }
+    
+    while(teams.length > 1) {
+      let randomNumber = Math.floor(Math.random() * teams.length)
+      const team1 = teams[randomNumber];
+      teams.splice(randomNumber, 1);
+      randomNumber = Math.floor(Math.random() * teams.length)
+      const team2 = teams[randomNumber];
+      schedule.push([team1.name, team2.name]);
+      teams.splice(randomNumber, 1);
+    }
+    console.log(schedule);
+    
   }
 
   private setLeagueTeam() {
