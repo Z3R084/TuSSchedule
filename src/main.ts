5eaba03774e6f5b549b868ae5cf79623974894e7
unknown <EckhardQ@QDTTW001.qdt.lan>
2017-12-29 12:48:11 +0100

    test succeeded

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,3 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-hallo
